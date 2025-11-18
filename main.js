677f6905499929c73a7b3a61b8d911c2ff64e068
Mareno Demmers <91221126+mhjdemmers@users.noreply.github.com>
2025-11-18 19:00:26 +0100

    refactor: streamline geometry creation and improve rendering setup

--- main.js
+++ main.js
@@ -4,19 +4,20 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 function main() {
     const canvas = document.querySelector('#c');
     const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+    renderer.setPixelRatio(window.devicePixelRatio);
 
+    // Camera
     const fov = 40;
     const aspect = 2;
     const near = 0.1;
     const far = 1000;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    
     camera.position.z = 150;
-
     const controls = new OrbitControls( camera, canvas );
     controls.target.set( 0,0,0 );
     controls.update();
     
+    // Scene
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0xAAAAAA);
     
@@ -28,19 +29,8 @@ function main() {
         scene.add(light);
     }
 
-    const objects = [];
-    const spread = 11;
-
-    function addObject(x, y, z, obj) {
-        obj.position.x = x * spread;
-        obj.position.y = y * spread;
-        obj.position.z = z * spread;
-
-        scene.add(obj);
-        objects.push(obj);
-    }
-
-    function createMaterial() {
+    // Helpers
+    const createMaterial = () => {
         const material = new THREE.MeshPhongMaterial({
             side: THREE.DoubleSide,
         });
@@ -53,27 +43,23 @@ function main() {
         return material;
     }
 
-    function addSolidGeometry(x, y, z, geometry) {
+    const addCubie = (x, y, z, size) => {
+        const geometry = new THREE.BoxGeometry(size, size, size);
         const mesh = new THREE.Mesh(geometry, createMaterial());
-        console.log("yeet");
-        addObject(x, y, z, mesh);
+        mesh.position.x = x * size;
+        mesh.position.y = y * size;
+        mesh.position.z = z * size;
+        scene.add(mesh);
     }
 
-    function addLineGeometry(x, y, z, geometry) {
-        const material = new THREE.LineBasicMaterial( { color: 0x000000 });
-        const mesh = new THREE.LineSegments( geometry, material );
-        addObject(x, y, z, mesh);
-    }
-
+    // Create 3*3*3 cube
     {
-        const width = 10;
-        const height = 10;
-        const depth = 10;
+        const size = 10;
        
         for (let x = -1; x <= 1; x++) {
             for (let y = -1; y <= 1; y++) {
                 for (let z = -1; z <= 1; z++) {
-                    addSolidGeometry(x, y, z, new THREE.BoxGeometry(width, height, depth));
+                    addCubie(x, y, z, size);
                 }
             }
         }
@@ -85,7 +71,7 @@ function main() {
         const height = canvas.clientHeight;
         const needResize = canvas.width !== width || canvas.height !== height;
         if (needResize) {
-        renderer.setSize(width, height, false);
+            renderer.setSize(width, height, false);
         }
         return needResize;
     }
@@ -98,13 +84,6 @@ function main() {
             camera.aspect = canvas.clientWidth / canvas.clientHeight;
             camera.updateProjectionMatrix();
         }
-
-        // objects.forEach((obj, ndx) => {
-        //     const speed = 1 + ndx * .1;
-        //     const rot = time * speed;
-        //     obj.rotation.x = rot;
-        //     obj.rotation.y = rot;
-        // });
 
         renderer.render(scene, camera);
 
